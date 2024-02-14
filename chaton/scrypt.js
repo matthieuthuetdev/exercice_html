3dcffd141be6114e78ffd4e181b42a84487638b9
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-02-14 12:39:09 +0100

    exercice js

--- chaton/scrypt.js
+++ chaton/scrypt.js
@@ -1,15 +1,16 @@
+const body = document.querySelector("body")
 function add_class() {
     const btn = document.getElementById("switch_font");
-    btn.classList.add("dis");
+    body.classList.add("dis");
   }
   
   function delete_class() {
-    const btn = document.getElementById("switch_font");
-    btn.classList.remove("dis");
+    const btn = document.querySelector("#switch_font");
+    body.classList.remove("dis");
   }
   
-  document.getElementById("switch_font").addEventListener("click", function() {
-    if (btn.classList.contains("dis")) {
+  document.querySelector("#switch_font").addEventListener("click", function() {
+    if (body.classList.contains("dis")) {
       delete_class();
     } else {
       add_class();
