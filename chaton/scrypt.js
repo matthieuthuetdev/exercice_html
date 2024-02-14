74da05995790d9e6dc47bcdcfe86dc062a68678e
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-02-14 16:30:34 +0100

    ajout de la case à cocher directement en js

--- chaton/scrypt.js
+++ chaton/scrypt.js
@@ -1,19 +1,17 @@
+const checkbox = document.createElement("input")
+checkbox.setAttribute("type","checkbox")
+checkbox.setAttribute("id","switch_font")
+const label = document.createElement("label")
+label.setAttribute("for","switch_font")
+label.textContent = "font open-dis"
+const span = document.createElement("span")
+span.appendChild(checkbox)
+span.appendChild(label)
+document.body.appendChild(span)
+
+
 const body = document.querySelector("body")
-function add_class() {
-    const btn = document.getElementById("switch_font");
-    body.classList.add("dis");
-  }
-  
-  function delete_class() {
-    const btn = document.querySelector("#switch_font");
-    body.classList.remove("dis");
-  }
-  
-  document.querySelector("#switch_font").addEventListener("click", function() {
-    if (body.classList.contains("dis")) {
-      delete_class();
-    } else {
-      add_class();
-    }
-  });
-  
+const btn = document.querySelector("#switch_font")
+.addEventListener("click", function () {
+  body.classList.toggle("dis");
+});
