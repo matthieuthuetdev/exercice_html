63b05fb36e588223d92d37f76e2c1b950572959d
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-02-15 10:03:48 +0100

    création du troisième article dynamiquement

--- chaton/scrypt.js
+++ chaton/scrypt.js
@@ -32,6 +32,16 @@ h2_2.textContent = "Wiskey"
 const p_2 = document.createElement("p")
 p_2.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit consequuntur nihil quia recusandae earum quidem numquam est distinctio ipsam ratione, blanditiis sequi, magni quo tempora aperiam sunt temporibus voluptatibus quis."
 // création du troisième article dynamiquement.
+const article_3 = document.createElement("article")
+article_3.setAttribute("class", "chat")
+const img3 = document.createElement("img")
+img3.setAttribute("src","./image/cat3.jpg")
+img3.setAttribute("alt","cette image représante un chat")
+const h2_3 = document.createElement("h2")
+h2_3.textContent = "Wiskey"
+const p_3 = document.createElement("p")
+p_3.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit consequuntur nihil quia recusandae earum quidem numquam est distinctio ipsam ratione, blanditiis sequi, magni quo tempora aperiam sunt temporibus voluptatibus quis."
+
 // affichage sur la page.
 header.appendChild(h1)
 span.appendChild(checkbox)
@@ -46,6 +56,11 @@ main.appendChild(article_2)
 article_2.appendChild(img2)
 article_2.appendChild(h2_2)
 article_2.appendChild(p_2)
+
+main.appendChild(article_3)
+article_3.appendChild(img3)
+article_3.appendChild(h2_3)
+article_3.appendChild(p_3)
 
 // traitement de la case à coché.
 const body = document.querySelector("body")
